cd1513416320fbc63927f5421a16bbc0296b5878
loucass003 <loucass003@gmail.com>
2019-05-25 16:18:39 +0200

    User connection check

--- src/controllers/users.php
+++ src/controllers/users.php
@@ -2,18 +2,26 @@
 
 include_once "models/order.php";
 
+function middleware($context, $args, $next)
+{
+	if (!isset($_SESSION['user']))
+	{
+		echo "You need to be connected";
+		return (FALSE);
+	}
+	$next($context, $args);
+	return (TRUE);	
+}
+
 function orders()
 {
-    if (isset($_SESSION['user']))
+    $orders = order_getorder($_SESSION['user']['id']);
+    $order_sum = [];
+    foreach($orders as $key => $order)
     {
-        $orders = order_getorder($_SESSION['user']['id']);
-        $order_sum = [];
-        foreach($orders as $key => $order)
-        {
-            $products = order_getproducts($order['id']);
-            foreach ($products as $product)
-                $order_sum[$key] += $product['price'] * $product['amount'];
-        }
+        $products = order_getproducts($order['id']);
+        foreach ($products as $product)
+            $order_sum[$key] += $product['price'] * $product['amount'];
     }
 	include "views/orders.php";
 }
